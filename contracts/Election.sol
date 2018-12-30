85955f8f3a0886389f92472960d1b47df8c9ed81
Bahaddin Yaşar <bahaddinyasar@gmail.com>
2018-12-29 21:52:20 -0800

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+  // store candidate
+  // read candidate
+  string public candidate;
+  // constructor
+  constructor() public {
+    candidate = "candidate 1";
+  }
+
+}
