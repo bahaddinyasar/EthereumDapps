6b312d1ba1b9dccca91e1253dd4b349ecc3ee11e
Bahaddin Yaşar <bahaddinyasar@gmail.com>
2018-12-31 21:41:39 -0800

    2. List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-  // store candidate
-  // read candidate
-  string public candidate;
-  // constructor
-  constructor() public {
-    candidate = "candidate 1";
-  }
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
 }
